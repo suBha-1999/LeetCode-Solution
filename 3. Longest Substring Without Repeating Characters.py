4a3a739753c4f4866f5ac960292b2f9fb2b4fe9d
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-05-30 21:58:56 +0530

    3. Longest Substring Without Repeating Characters.py

---
+++ 3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,23 @@
+import collections
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
+        count = collections.Counter()
+
+        l = 0
+        for r, c in enumerate(s):
+            count[c] += 1
+            while count[c] > 1:
+                count[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+
+        return ans
+
+sol = Solution()
+
+s = "abcabcbb"
+result = sol.lengthOfLongestSubstring(s)
+print(result)
