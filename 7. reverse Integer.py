fbc75885bb7cc74ed7e96f2266d2b1c4cd91f5dd
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-06-02 22:48:55 +0530

    reverse Integer.py

---
+++ 7. reverse Integer.py
@@ -0,0 +1,23 @@
+
+import math
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if( x >= 0):
+            val = int(str(x)[::-1])
+        else:
+            val = -(int(str(abs(x))[::-1]))
+
+        if(val <= -2**31 or val >= 2**31 - 1):
+            return 0
+        else:
+            return val
+
+
+## driver code
+sol = Solution()
+
+x = 123
+res = sol.reverse(x)
+print(res)
