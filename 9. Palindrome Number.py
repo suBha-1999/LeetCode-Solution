73674a31775d12f9fd19c226dd38290939769e21
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-06-03 23:36:03 +0530

    Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        rev = s[::-1]
+        if s==rev:
+            return True
+        else:
+            return False
+
+sol = Solution()
+
+X = 121 # -212
+print(sol.isPalindrome(X))
