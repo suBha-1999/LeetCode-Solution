698e9a942b7dd4f852d88cf4c73ee876512f4fa0
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-06-07 13:38:19 +0530

    16. 3Sum Closest.py

---
+++ 16. 3Sum Closest.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        ans = nums[0] + nums[1] + nums[2]
+
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            # Choose nums[i] as the first number in the triplet, then search the
+            # remaining numbers in [i + 1, n - 1].
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                summ = nums[i] + nums[l] + nums[r]
+                if summ == target:
+                    return summ
+                if abs(summ - target) < abs(ans - target):
+                    ans = summ
+                if summ < target:
+                    l += 1
+                else:
+                    r -= 1
+
+        return ans
+
+sol = Solution()
+input = [-1,2,1,-4]
+target = 1
+result = sol.threeSumClosest(input, target)
+print(result)
