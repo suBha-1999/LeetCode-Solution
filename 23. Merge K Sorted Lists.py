bbd431f0d428b3fc07c528821c5ebf48f7dc0045
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-06-12 22:36:47 +0530

    23. Merge K Sorted Lists.py

---
+++ 23. Merge K Sorted Lists.py
@@ -0,0 +1,68 @@
+
+from typing import List, Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
+
+# Helper to convert list of values into a linked list
+def build_linked_list(arr):
+    dummy = ListNode()
+    current = dummy
+    for val in arr:
+        current.next = ListNode(val)
+        current = current.next
+    return dummy.next
+
+# Helper to print linked list
+def print_linked_list(node):
+    res = []
+    while node:
+        res.append(node.val)
+        node = node.next
+    return res
+
+
+sol = Solution()
+
+Inp = [[1,4,5],[1,3,4],[2,6]]
+linked_lists = [build_linked_list(lst) for lst in Inp]
+
+result = sol.mergeKLists(linked_lists)
+print(print_linked_list(result))
