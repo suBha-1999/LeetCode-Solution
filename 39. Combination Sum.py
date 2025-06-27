a9eb27ebd25e2982f88592409b7f74299e4be283
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-06-27 23:23:19 +0530

    39. Combination Sum.py

---
+++ 39. Combination Sum.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+
+            for j in range(i, len(nums)):
+                if total + nums[j] > target:
+                    return
+                cur.append(nums[j])
+                dfs(j, cur, total + nums[j])
+                cur.pop()
+
+        dfs(0, [], 0)
+        return res
+
+sol = Solution()
+candidates = [2,3,6,7]
+target = 7
+print(sol.combinationSum(candidates, target))
