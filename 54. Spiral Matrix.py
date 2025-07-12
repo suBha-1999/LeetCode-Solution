7742f6b30cce9a457f1224a0eed83e85cd19410d
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-07-12 19:41:59 +0530

    54. Spiral Matrix.py

---
+++ 54. Spiral Matrix.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        steps = [len(matrix[0]), len(matrix) - 1]
+
+        r, c, d = 0, -1, 0
+        while steps[d & 1]:
+            for i in range(steps[d & 1]):
+                r += directions[d][0]
+                c += directions[d][1]
+                res.append(matrix[r][c])
+            steps[d & 1] -= 1
+            d += 1
+            d %= 4
+        return res
+    
+sol = Solution()
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(sol.spiralOrder(matrix))
