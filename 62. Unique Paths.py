9e08e3552115b007f7df622d378491f6b6a1a1ef
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-07-20 19:24:16 +0530

    62.Unique Paths.py

---
+++ 62. Unique Paths.py
@@ -0,0 +1,15 @@
+class Solution:
+  def uniquePaths(self, m: int, n: int) -> int:
+    dp = [1] * n
+
+    for _ in range(1, m):
+      for j in range(1, n):
+        dp[j] += dp[j - 1]
+
+    return dp[n - 1]
+  
+
+sol = Solution()
+m = 3
+n = 7
+print(sol.uniquePaths(m, n))
