dcb3a10eae068ef167c565f0bca219477d5ffc1b
SUBHA1999 <subhajitporyaon5@gmail.com>
2025-07-28 22:00:25 +0530

    70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        sqrt5 = math.sqrt(5)
+        phi = (1 + sqrt5) / 2
+        psi = (1 - sqrt5) / 2
+        n += 1
+        return round((phi**n - psi**n) / sqrt5)
+    
+sol = Solution()
+n = 3
+print(sol.climbStairs(n))
